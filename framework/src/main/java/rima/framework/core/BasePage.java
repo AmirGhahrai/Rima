d82ad95ff57378f24d32bc27952372207632874a
Amir Gharai <Amir.Gharai@AML0084.local>
2017-10-05 17:23:36 +0100

    BasePage.java

---
+++ framework/src/main/java/rima/framework/core/BasePage.java
@@ -0,0 +1,21 @@
+package rima.framework.core;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BasePage {
+
+    private static final int TIMEOUT = 5; //seconds
+    private static final int POLLING = 100; //milliseconds
+
+    protected WebDriver driver;
+    private WebDriverWait wait;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, TIMEOUT, POLLING);
+        PageFactory.initElements(new AjaxElementLocatorFactory(driver, TIMEOUT), this);
+    }
+}
