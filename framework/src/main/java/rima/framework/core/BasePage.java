a77ab1c53ac25c3820d419d45fdba414cb610de5
AmirAmido <amir.ghahrai@amido.com>
2018-01-04 10:40:41 +0000

    added BasePage and BaseTest plus sample page object and sample test page

--- framework/src/main/java/rima/framework/core/BasePage.java
+++ framework/src/main/java/rima/framework/core/BasePage.java
@@ -1,8 +1,10 @@
 package rima.framework.core;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage {
@@ -17,5 +19,17 @@ public class BasePage {
         this.driver = driver;
         wait = new WebDriverWait(driver, TIMEOUT, POLLING);
         PageFactory.initElements(new AjaxElementLocatorFactory(driver, TIMEOUT), this);
+    }
+
+    protected void waitForElementToAppear(By locator) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    protected void waitForElementToDisappear(By locator) {
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(locator));
+    }
+
+    protected void waitForTextToDisappear(By locator, String text) {
+        wait.until(ExpectedConditions.not(ExpectedConditions.textToBe(locator, text)));
     }
 }
